d0866f6cc26a747353348c786ca14fb151113a95
Karina Hutula <khutula@g.clemson.edu>
2021-06-22 19:48:49 -0400

    add second ICA for Python Lesson 1

---
+++ 03-Python/Lesson 03.1/06-Stu_DownToInput/Unsolved/DownToInput.py
@@ -0,0 +1,8 @@
+my_name = input("What is your name? ")
+neighbor_name = input("What is your neighbor's name? ")
+
+my_code = int(input("How many months have you been coding? "))
+neighbor_code = int(input("How many months has your neighbor been coding? "))
+
+print("I am " + my_name + ", and my neighbor is " + neighbor_name)
+print(f"Together we have been coding for {my_code + neighbor_code} months!")
