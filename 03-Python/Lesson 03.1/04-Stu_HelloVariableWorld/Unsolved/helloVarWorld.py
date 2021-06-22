2b33b1f05eed4b5440f9d5dce2362b36b5f8b91a
Karina Hutula <khutula@g.clemson.edu>
2021-06-22 19:31:31 -0400

    add first ICA for Python lesson 1

---
+++ 03-Python/Lesson 03.1/04-Stu_HelloVariableWorld/Unsolved/helloVarWorld.py
@@ -0,0 +1,12 @@
+name = "Karina"
+country = "United States"
+age = 26
+hourly_wage = 38
+satisfied = False
+daily_wage = hourly_wage * 8
+
+print("Hello " + name +"!")
+print("You live in "+country)
+print(f"You are {age} years old")
+print(f"You make {daily_wage} per day")
+print(f"Are you satisfied with your current wage? {satisfied}")
