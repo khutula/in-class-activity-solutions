88a5ab747ff4ceb28a78d5e33b93186e2673ce98
Karina Hutula <khutula@g.clemson.edu>
2021-06-24 18:41:39 -0400

    add first ICA for Python lesson 2

---
+++ 03-Python/Lesson 03.2/01-Stu_QuickCheckup/Solved/quick_check_up.py
@@ -0,0 +1,19 @@
+# Quick Check-Up!
+
+# Print Hello User!
+print("Hello User!")
+
+# Take in User Input
+user_name = input("What is your name? ")
+
+# Respond Back with User Input
+print("Hello " + user_name)
+
+# Take in the User Age
+user_age = int(input("What is your age? "))
+
+# Respond Back with a statement based on age
+if user_age >= 30:
+    print("Ah... A well traveled soul are ye.")
+else: 
+    print("Aww... you're just a baby!")
