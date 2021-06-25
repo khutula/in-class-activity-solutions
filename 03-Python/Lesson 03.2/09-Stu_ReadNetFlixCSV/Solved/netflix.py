c0a1253b63a99332cecc3b285236b642bcc33efa
Karina Hutula <khutula@g.clemson.edu>
2021-06-24 20:57:45 -0400

    add comments to code of fifth ICA solution

--- 03-Python/Lesson 03.2/09-Stu_ReadNetFlixCSV/Solved/netflix.py
+++ 03-Python/Lesson 03.2/09-Stu_ReadNetFlixCSV/Solved/netflix.py
@@ -1,18 +1,31 @@
+#import modules
 import os
 import csv
 
+# set path for file
 csvpath = os.path.join("..","Resources","netflix_ratings.csv")
 
+#prompt user for video lookup
 search = input("What show or movie are you looking for? ")
+
+#set variable to check if we found the video
 found = False
 
+# open the csv
 with open(csvpath, newline = "") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
+
+    # loop through looking through video
     for row in csvreader:
         if row[0] == search:
-            print(f"{search} is rated {row[1]} with a rating of {row[3]}.") 
+            print(f"{search} is rated {row[1]} with a rating of {row[5]}.") 
+
+            # set variable to true that we found the video
             found = True
+
+            # stop searching after first result returned
             break
 
+#print this statement if the video cannot be found
 if found == False:
     print("Your video could not be found.")
